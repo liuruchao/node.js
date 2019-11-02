e4d8ed6614dff209ed50c0328c0512027ec9cd28
liuruchao <runlrc@163.com>
2019-11-02 23:40:19 +0800

    feat: node 预习知识

---
+++ geek__learn-node/event.js
@@ -0,0 +1,27 @@
+const eventLoop = {
+  queue: [],
+  loop() {
+    while (this.queue.length) {
+      var callback = this.queue.shift()
+      callback()
+    }
+    setTimeout(this.loop.bind(this), 50)
+  },
+  add(callback) {
+    this.queue.push(callback)
+  }
+}
+
+eventLoop.loop()
+
+setTimeout(() => {
+  eventLoop.add(function() {
+    console.log(1)
+  })
+}, 500)
+
+setTimeout(() => {
+  eventLoop.add(function() {
+    console.log(2)
+  })
+}, 800)
